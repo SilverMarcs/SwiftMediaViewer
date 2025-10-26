791d19332bb399effd515312b23bc23a8aac1d2e
Zabir Raihan <zabirraihan@icloud.com>
2025-10-27 00:16:30 +0800

    single image only for smvimage

--- Sources/SwiftMediaViewer/Image/SMVImage.swift
+++ Sources/SwiftMediaViewer/Image/SMVImage.swift
@@ -8,67 +8,34 @@
 import SwiftUI
 
 public struct SMVImage: View {
-    // TODO: ask for url
-    private let urls: [URL]
+    private let url: URL
     private let targetSize: Int
 
     @Namespace private var imageNamespace
     @State private var showFullscreen = false
 
     public init(url: URL, targetSize: Int) {
-        self.urls = [url]
-        self.targetSize = targetSize
-    }
-
-    public init(urls: [URL], targetSize: Int) {
-        self.urls = urls
-        self.targetSize = targetSize
-    }
-    
-    public init(urlString: String, targetSize: Int) {
-        if let url = URL(string: urlString) {
-            self.urls = [url]
-        } else {
-            self.urls = []
-        }
-        self.targetSize = targetSize
-    }
-    
-    public init(urlStrings: [String], targetSize: Int) {
-        self.urls = urlStrings.compactMap { URL(string: $0) }
+        self.url = url
         self.targetSize = targetSize
     }
 
     public var body: some View {
-        if let firstURL = urls.first {
-            CachedAsyncImage(url: firstURL, targetSize: targetSize)
-                #if !os(macOS)
-                .matchedTransitionSource(id: firstURL, in: imageNamespace)
-                #endif
-                .contentShape(Rectangle())
-                .onTapGesture {
-                    showFullscreen = true
-                }
-                .overlay(alignment: .bottomTrailing) {
-                    if urls.count > 1 {
-                        Text("+\(urls.count - 1)")
-                            .font(.caption2)
-                            .fontWeight(.semibold)
-                            .foregroundStyle(.white)
-                            .padding(8)
-                            .background(Color.black.opacity(0.7))
-                            .cornerRadius(6)
-                            .padding(12)
-                    }
-                }
-                .conditionalFullScreen(isPresented: $showFullscreen) {
-                    SMVImageModal(
-                        urls: urls,
-                        startIndex: 0,
-                        targetSize: targetSize,
-                        namespace: imageNamespace
-                    )
-                }
-        }
+        CachedAsyncImage(url: url, targetSize: targetSize)
+            #if !os(macOS)
+            .matchedTransitionSource(id: url, in: imageNamespace)
+            #endif
+            .contentShape(Rectangle())
+            .onTapGesture {
+                showFullscreen = true
+            }
+            .conditionalFullScreen(isPresented: $showFullscreen) {
+                SMVImageModal(
+                    urls: [url],
+                    startIndex: 0,
+                    targetSize: targetSize,
+                    namespace: imageNamespace
+                )
+            }
+        
     }
 }
